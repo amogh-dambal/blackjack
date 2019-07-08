01d31ea3f1f11d2e59fd072b8acc3aa974e466f0
Amogh Dambal <amdam2013@gmail.com>
2019-07-07 19:50:51 -0500

    separate implementation

--- Headers/Player.h
+++ Headers/Player.h
@@ -13,30 +13,12 @@ enum class Status
 
 class Player
 {
-    Player() : status(Status::Under) {};
+    Player();
 
-    std::pair<int, int> score() const
-    {
-        int sum = 0;
-        int sum_ace = 0;
-        for (Card c : hand)
-        {
-            sum += static_cast<int>(c.val);
-            if (c.val == ACE)
-            {
-                sum_ace += 11;
-            }
-            else
-            {
-                sum_ace += static_cast<int>(c.val);
-            }
-            
-        }
-        return std::make_pair(sum, sum_ace);
-    };
+    std::pair<int, int> score() const;
 
     Status status;
-    std::pair<int, int> score;
     PlayerType type;
     std::vector<Card> hand;
+    
 };
